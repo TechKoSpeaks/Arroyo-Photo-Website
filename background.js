b8c382993369dbf0b4470365a89fd46e2154cb01
Travis Ko <tkospeaks@gmail.com>
2021-05-04 17:20:25 -0700

    Added new styling, transitions

---
+++ background.js
@@ -0,0 +1,11 @@
+$('.bgSwitch').bgswitcher({
+    images: ["background.jpg","backshot.jpg","aleanddru.jpg"],
+    effect: "fade",
+    interval: 10000
+});
+
+$(".wrapper").bgswitcher({
+    images: ["background.jpg","backshot.jpg","aleanddru.jpg"],
+    effect: "fade",
+    interval: 10000
+  });
